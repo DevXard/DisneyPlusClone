695cfb2ab551802ae5eb1603faae260401424826
Pavlin <forex.xard@gmail.com>
2021-04-21 23:23:16 -0400

    Created navbar and navbar navigation links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Header from './components/header/Header';
 import Login from './components/Login/Login';
 import './App.css';
 
@@ -6,6 +8,7 @@ function App() {
   return (
     <div className="bg-gray-900">
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
